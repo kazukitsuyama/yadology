a92da67dc88998db9c527ecd508b274346c65b63
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-06-24 23:51:47 +0900

    Bootstrap導入

--- db/migrate/20210619142226_create_tweets.rb
+++ db/migrate/20210619142226_create_tweets.rb
@@ -1,7 +1,12 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-
+      t.string     :yado_title
+      t.string     :yado_name
+      t.text       :text
+      t.integer    :date
+      t.integer    :area
+      t.integer    :price 
       t.timestamps
     end
   end
