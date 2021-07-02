76a829e3cc1bc4845b760cfed7b1b6835d0e3508
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-07-03 00:30:33 +0900

    tweetテーブル作成

--- db/migrate/20210619142226_create_tweets.rb
+++ db/migrate/20210619142226_create_tweets.rb
@@ -1,12 +1,13 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-      t.string     :yado_title
-      t.string     :yado_name
-      t.text       :text
-      t.integer    :date
-      t.integer    :area
-      t.integer    :price 
+      t.string     :yado_title,         null: false
+      t.string     :yado_name,          null: false
+      t.text       :text,               null: false
+      t.integer    :date,               null: false
+      t.integer    :area,               null: false
+      t.integer    :price,              null: false
+      t.references :user,               null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_27_134417) do
+ActiveRecord::Schema.define(version: 2021_06_19_142226) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "yado_title"
-    t.string "yado_name"
-    t.text "text"
-    t.integer "date"
-    t.integer "area"
-    t.integer "price"
+    t.string "yado_title", null: false
+    t.string "yado_name", null: false
+    t.text "text", null: false
+    t.integer "date", null: false
+    t.integer "area", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_06_27_134417) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
