800310f4e12d48a98573e2836e71b7c25dd9bf89
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-07-03 01:31:44 +0900

    バリデーションの記述

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,18 @@
 class Tweet < ApplicationRecord
+  with_options presence: true do
+    validates :image
+    validates :yado_title
+    validates :yado_name
+    validates :text
+    validates :date
+    validates :price
+  end
+
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :area
+  end
+
+
   belongs_to :user
   has_one_attached :image
 end
