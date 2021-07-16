d525a7aa5649a277c3dcfcebcc561edb3d57e8e8
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-07-16 16:29:04 +0900

    コメント機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -33,6 +33,7 @@ class TweetsController < ApplicationController
 
   def show
     @tweet = Tweet.find(params[:id])
+    @comment = Comment.new
     @comments = @tweet.comments.includes(:user)
   end
 
