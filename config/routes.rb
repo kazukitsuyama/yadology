3e4ac43f4cbaeb35e1a8b1833e8e14a01c4d83fc
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-07-26 21:31:15 +0900

    routes編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'tweets/index'
   root to: "tweets#index"
+  get 'tweets/search'
   resources :tweets do
     resources :comments, only: [:new, :create]
   end
