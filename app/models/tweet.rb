5d3042482218d3fd1768803cb6ac50708c982d90
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-07-28 16:49:18 +0900

    アソシエーションの定義

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -15,6 +15,8 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :comments
+  has_many :likes
+  has_many :liked_users, through: :likes, source: :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :area
