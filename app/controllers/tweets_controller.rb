8014868a7531737a1e01e1c57d2e21c5e3c9a748
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-08-18 00:11:44 +0900

    ゲストログイン機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -53,6 +53,15 @@ class TweetsController < ApplicationController
     @results = @q.result
   end
 
+  def new_guest
+    user = User.find_or_create_by(email: 'guest@example.com') do |user|
+    user.name = "ゲスト"
+    user.password = SecureRandom.alphanumeric(10) + [*'a'..'z'].sample(1).join + [*'0'..'9'].sample(1).join
+    end
+    sign_in user
+    redirect_to root_path
+  end
+
   private
 
   def tweet_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'tweets/index'
   root to: "tweets#index"
+  post '/tweets/guest_sign_in', to: 'tweets#new_guest'
   resources :tweets do
     collection do
       get 'search'
